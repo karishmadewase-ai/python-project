c3bc94828e2421670b24938d955f87ee7b06edf3
karishmadewase-ai <karishmadewase@gmail.com>
2026-03-04 14:04:43 +0530

    Add files via upload

---
+++ python.1.py
@@ -0,0 +1,12 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Simple Python Program
+... 
+... a = 5
+... b = 3
+... 
+... sum = a + b
+... 
+... print("The sum is:", sum)
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
